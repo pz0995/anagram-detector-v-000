21b4e1b167fe77b5a5800c52efb9839a4a7f6568
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-25 09:37:38 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @name = word
+  end
+
+  def matches(collection)
+    words.each { |x, y| p y }
+  end
+end
