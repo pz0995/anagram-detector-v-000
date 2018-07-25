0230d6f578e2aa6dd91a24ca702d0a19e9830a19
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-25 09:44:52 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,7 +4,6 @@ class Anagram
     @word = word
   end
 
-  def are_anagrams?(words)
-          words.map { |w| sort_alphabetically(w) }.uniq.size == 1
-      end
+  words_array.select do |element|
+    (@word.split("").sort) == (element.split("").sort)
 end
